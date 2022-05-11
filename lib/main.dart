7ce4df362aa4ba5711cbe441a43ad54b5b06b8a4
Sylvia Kang <sylvia@ksvs.kh.edu.tw>
2022-05-11 12:10:48 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const FlashChat());
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
